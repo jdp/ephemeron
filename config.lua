660cd60f514098b83213356d7a6058ca8859ae45
jdp <jdp34@njit.edu>
2010-09-07 07:59:55 -0400

    localhost => 127.0.0.1 to play nice with 0mq

--- config.lua
+++ config.lua
@@ -1,13 +1,17 @@
 protocol = "tcp"
-host = "localhost"
+host = "127.0.0.1"
 port = 5555
 
 ttl_extend = 3600
 
 function on_get(key, value)
-  print(string.format("GET {\"%s\":\"%s\"}", key, value))
+	print(string.format("GET {\"%s\":\"%s\"}", key, value))
 end
 
 function on_set(key, value)
-  print(string.format("SET {\"%s\":\"%s\"}", key, value))
+	print(string.format("SET {\"%s\":\"%s\"}", key, value))
+end
+
+function on_expire(key, value)
+	print(string.format("EXPIRED {\"%s\":\"%s\"}", key, value))
 end
